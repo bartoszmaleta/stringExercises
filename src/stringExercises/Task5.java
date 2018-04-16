eaa45743e528728d3b32e2de2629d0eaa6bdf60e
Bartosz Maleta <bartosz.maleta@gmail.com>
2018-04-16 21:16:38 +0200

    Exe4 --> same as 2, Exe5 made --> shows the second letter from the end of the word

---
+++ src/stringExercises/Task5.java
@@ -0,0 +1,24 @@
+package stringExercises;
+
+import java.util.Scanner;
+
+public class Task5
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Podawj wyraz");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+
+        // 012345
+        // Michał
+
+//        System.out.println(s.charAt(s.length()-2));
+
+        int length = word.length();
+        System.out.println(word.charAt(length-2));
+
+
+
+    }
+}
