410a62a54f4bc62f7e40f8e79bf0c38ea388fd4b
Bartosz Maleta <bartosz.maleta@gmail.com>
2018-04-16 20:44:01 +0200

    Exercise1 completed: shows Your firstname and lastname

---
+++ src/Main/Main.java
@@ -0,0 +1,17 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Podaj imię i nazwisko");
+        Scanner scanner = new Scanner(System.in);
+        String firstname = scanner.next();
+        String lastname = scanner.next();
+        System.out.println("Twoje imię i nazwisko to: " + firstname + " " + lastname);
+
+    }
+
+}
