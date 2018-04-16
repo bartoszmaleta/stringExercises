b350ce34a819e7b17cc6d8f7bf0ba5f543027b8c
Bartosz Maleta <bartosz.maleta@gmail.com>
2018-04-16 21:02:32 +0200

    Exe3 made --> shows quote, tricky because of the regex """"

---
+++ src/stringExercises/Task3.java
@@ -0,0 +1,10 @@
+package stringExercises;
+
+public class Task3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\"Mowa jest srebrem, a milczenie złotem\"");
+        
+    }
+}
